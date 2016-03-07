57db0313c330143527a40769b76098a3c01bf098
Heather Miller <heather.miller@epfl.ch>
2016-03-07 12:50:38 +0100

    Most of website ready

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+---
+---
+
+/***************************
+ * Document initialization
+ **************************/
+
+$(document).ready(function(){
+  // background image on frontpage
+  $(".splash").backstretch("{{ site.baseurl }}/resources/img/lakegeneva-gradientmapped.jpg");
+});
