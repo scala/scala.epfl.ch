605789d34016c947c65d77621c11fbb2bed5dd96
Heather Miller <heather.miller@epfl.ch>
2016-03-08 08:49:05 +0100

    Almost there

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,6 +6,5 @@
  **************************/
 
 $(document).ready(function(){
-  // background image on frontpage
-  $(".splash").backstretch("{{ site.baseurl }}/resources/img/lakegeneva-gradientmapped.jpg");
+
 });
