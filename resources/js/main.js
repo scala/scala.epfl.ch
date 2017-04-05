2c8a192a6e0f9fc869bd5b6d76f0b8463109befb
José Alcántara <jose@trendix.es>
2017-04-05 17:52:00 +0200

    Project screen search functionality.

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,5 +6,20 @@
  **************************/
 
 $(document).ready(function(){
-
+    var search = $('.search input');
+    console.log(search);
+    search.keyup(function() {
+        console.log('key');
+        var value = $(this).val();
+        value = value ? value.toLowerCase() : null;
+        $('.project').each(function () {
+            var title = $(this).find('h2').text();
+            var text = $(this).find('.content').text();
+            if(value && (text.toLowerCase().indexOf(value) == -1 && title.toLowerCase().indexOf(value) == -1)) {
+                $(this).css('display', 'none');
+            } else {
+                $(this).css('display', 'block');
+            }
+        });
+    });
 });
