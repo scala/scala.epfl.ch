93cc0a6a59592f45eacea854c14f20cf17a130a5
José Alcántara <jose@trendix.es>
2017-05-02 13:06:53 +0200

    Added new project description modals.

--- resources/js/main.js
+++ resources/js/main.js
@@ -16,10 +16,32 @@ $(document).ready(function(){
             var title = $(this).find('h2').text();
             var text = $(this).find('.content').text();
             if(value && (text.toLowerCase().indexOf(value) == -1 && title.toLowerCase().indexOf(value) == -1)) {
-                $(this).css('display', 'none');
+                $(this).parent().css('display', 'none');
             } else {
-                $(this).css('display', 'block');
+                $(this).parent().css('display', 'block');
             }
         });
+    });
+
+    /* PROJECT DESCRIPTION MODALS */
+    $(document).on('click', 'a.read-more', function(e) {
+        e.preventDefault();
+        var content = $(this).closest('.project').html();
+        var container = $('#modal-description .project');
+        container.html(content);
+        container.find('.references').append('<button type="button" class="close" data-dismiss="modal" aria-label="Close"><i class="fa fa-times"></i></button>');
+        container.find('.content').remove();
+        container.find('.modal-content-text').removeClass('hidden');
+        $('#modal-description').modal('show');
+    });
+
+    /* PROJECT CONTRIBUTOR MODALS */
+    $(document).on('click', 'a.contributors-modal', function(e) {
+        e.preventDefault();
+        var target = $(this).data('target');
+        $('#modal-description').on('hidden.bs.modal', function () {
+            $(target).modal('show');
+        });
+        $('#modal-description').modal('hide');
     });
 });
