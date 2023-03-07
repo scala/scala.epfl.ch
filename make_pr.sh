9a8fe822313679211cddbc34184414212b43053b
Jamie Thompson <bishbashboshjt@gmail.com>
2023-03-07 15:30:27 +0100

    Update from https://github.com/scalacenter/5yimpactreport/commit/66b231c

---
+++ make_pr.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+GITHUB_REPOSITORY='scalacenter/5yimpactreport'
+GITHUB_SHA='66b231c'
+
+INPUT_DESTINATION_HEAD_BRANCH="update-5yimpactreport-2023-03-07_01h15m35s"
+INPUT_DESTINATION_BASE_BRANCH="main"
+
+if [ -z "$INPUT_PULL_REQUEST_REVIEWERS" ]
+then
+  PULL_REQUEST_REVIEWERS=$INPUT_PULL_REQUEST_REVIEWERS
+else
+  PULL_REQUEST_REVIEWERS="-r $INPUT_PULL_REQUEST_REVIEWERS"
+fi
+
+echo "Adding git commit"
+git add .
+if git status | grep -q "Changes to be committed"
+then
+  COMMIT_MESSAGE="Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+
+  if [ -z "$INPUT_PULL_REQUEST_MESSAGE" ]
+  then
+    PULL_REQUEST_MESSAGE=$COMMIT_MESSAGE
+  else
+    PULL_REQUEST_MESSAGE="$COMMIT_MESSAGE\n\n$INPUT_PULL_REQUEST_MESSAGE"
+  fi
+
+  git commit --message "$COMMIT_MESSAGE"
+  echo "Pushing git commit"
+  git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
+  echo "Creating a pull request"
+  gh pr create -t $INPUT_DESTINATION_HEAD_BRANCH \
+               -b $PULL_REQUEST_MESSAGE \
+               -B $INPUT_DESTINATION_BASE_BRANCH \
+               -H $INPUT_DESTINATION_HEAD_BRANCH \
+                  $PULL_REQUEST_REVIEWERS
+else
+  echo "No changes detected"
+fi
